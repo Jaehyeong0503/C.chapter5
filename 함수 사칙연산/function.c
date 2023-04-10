590a451acd209af8e01aa8cba571e03cd4d0cf6d
user <user@DESKTOP-37G5MQD>
2023-04-10 11:55:38 +0900

    초기 커밋.

---
+++ 함수 사칙연산/function.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function.h"
+
+
+void p(const char* msg, int n) {
+	printf(">>>>>>>>>>>>>>\n");
+	printf("%s 출력 : %d\n", msg, n);
+	printf(">>>>>>>>>>>>>>\n\n");
+}
+
+int showMenu(int n) {
+	int input;
+	printf("숫자 %d 입력: ", n); scanf_s("%d", &input);
+	return input;
+}
---
+++ 함수 사칙연산/function.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void p(const char*, int);
+int showMenu(int);
+
+
